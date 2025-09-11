06d3149c17fcb40bf155002dcc41a0bee1b07948
Trademark <wup404@gmail.com>
2025-09-11 09:27:24 +0200

    k

--- elso.py
+++ elso.py
@@ -1,3 +1,12 @@
 import random
 
 szamok=[]
+
+#lista feltoltese 100db random 2jegyu egesz szammal
+for i in range (100):
+    szam=random.randint(10,99)
+    szamok.append(szam)
+    
+#ell
+print(len(szamok))
+print(szamok) 
