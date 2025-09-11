782f6c928b35b75ff8ffc75bae256477460a919d
Trademark <wup404@gmail.com>
2025-09-11 10:35:27 +0200

    faszaAlapjatek

--- elso.py
+++ elso.py
@@ -15,16 +15,27 @@ jatek_szam=0
 nem_talalDB=0
 
 kitalalando_szam=szamok[random.randint(0, len(szamok))]
-
 jatszol=True
 while (jatszol):
-    tipp=int(input("tipped?: "))
+    tipp_sz=input("tipped?: ").strip()
+    if (tipp_sz.isdecimal()):
+        tipp=int(tipp_sz)
+    else:
+        print("egész számmal játsz")
+        continue
     
     while (tipp!=kitalalando_szam):
-        tipp=int(input("tipped?: "))
+        tipp_sz=input("tipped?: ").strip()
+        if (tipp_sz.isdecimal()):
+            tipp=int(tipp_sz)
+        else:
+            print("egész számmal játsz")
+            continue
         
     print("eltaláltad")
     
     folytat=input("akarsz meg jatszani? [I/N]")
     if (folytat=="N"):
         jatszol=False
+        
+
