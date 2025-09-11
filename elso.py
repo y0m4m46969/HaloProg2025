4dfa17e4e9d376f8ae9926bd41f89033de6f66e5
Trademark <wup404@gmail.com>
2025-09-11 09:08:19 +0200

    versio 1

--- elso.py
+++ elso.py
@@ -1,31 +1,3 @@
 import random
 
 szamok=[]
-
-#lista feltoltese 100db random 2jegyu egesz szammal
-for i in range (100):
-    szam=random.randint(10,99)
-    szamok.append(szam)
-    
-#ell
-print(len(szamok))
-print(szamok) 
-
-
-#EGYSZÁM JáTÉK
-jatek_szam=0
-nem_talalDB=0
-
-kitalalando_szam=szamok[random.randint(len(szamok))]
-tipp=int(input("tipped?: "))
-while (tipp != kitalalando_szam):
-    tipp=int(input("tipped?: "))
-    
-print("eltaláltad")
-folytat=input("akarsz meg jatszani? [I/N]")
-
-if (folytat=="I"):
-    #????
-    
-else:
-    exit()
